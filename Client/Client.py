2508aea0903282bda940ae3cd6dac7e24529a5ab
SnexyGuy <yazabeha2003@gmail.com>
2024-02-29 00:27:13 +0200

    first succeed dynamic image resize

--- Client/Client.py
+++ Client/Client.py
@@ -1,56 +1,45 @@
 import tkinter as tk
-#from win32api import GetSystemMetrics
-#from tkinter import ttk
+from PIL import ImageGrab, ImageTk, ImageOps, Image
 
-#screen_width, screen_height = GetSystemMetrics(0), GetSystemMetrics(1)
+def resize(event):
 
-from PIL import ImageGrab, ImageTk
+    display = ImageGrab.grab()
 
+    display.thumbnail((event.width,event.height))
 
-class Client_Window(tk.Tk):
-    def __init__(self,title,size):
-        super().__init__()
-        self.title(title)
-        self.geometry(f'{size[0]}x{size[1]}')
-        self.minsize(size[0],size[1])
+    display_img=ImageTk.PhotoImage(display)
 
+    new_width=event.width-display_img.width()
+    new_height=event.height-display_img.height()
 
+    event.widget.itemconfigure(screen_image,image=display_img)
+    event.widget.moveto(screen_image, new_width/2, new_height/2)
 
-        self.Controlling_client_frame=Controlling_client_frame(self)
-
-
-        self.mainloop()
-
-
-class Controlling_client_frame(tk.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.parent=parent
-
-
-
-        img = ImageGrab.grab()
-        img2 = ImageTk.PhotoImage(img)
-
-
-        self.screen = tk.Canvas(self,bg='green')
-
-
-
-        self.screen.create_image(0,0,image=img2,anchor='nw')
-        self.screen.image=img2
-
-        self.screen.pack(fill='both', expand=True)
-        self.pack(fill='both',expand=True)
+    event.widget.image=display_img
+    print(f'{event.width}x{event.height}')
 
 
 
 
+if __name__ == "__main__":
+    root=tk.Tk()
 
 
+    Controlling_client_frame=tk.Frame(root)
+    global display
+    display = ImageGrab.grab()
+    global display_img
+    display_img=ImageTk.PhotoImage(display)
 
-Client_Window('hi',(500,500))
+
+    displayed_screen=tk.Canvas(Controlling_client_frame,bg='green')
+    displayed_screen.update()
+    screen_image=displayed_screen.create_image(displayed_screen.winfo_reqwidth()/2,displayed_screen.winfo_reqheight()/2,image=display_img, anchor='center')
+    displayed_screen.image=display_img
+
+    displayed_screen.pack(fill='both', expand=True)
+    Controlling_client_frame.pack(fill='both',expand=True)
 
 
-
-#i have to work on dynamic image sizing in relation to window size
+    displayed_screen.bind('<Configure>',resize)
+    root.mainloop()
